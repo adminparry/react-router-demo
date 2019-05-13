27f594d4fc4dcd152905ca07fabff134aa509508
pparry <1170015014@qq.com>
2019-05-13 23:04:48 +0800

    concfit

--- react-redux-demo/src/index.js
+++ react-redux-demo/src/index.js
@@ -1,13 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-<<<<<<< HEAD
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
-
-=======
 
 import Routers from './Router';
 import * as serviceWorker from './serviceWorker';
@@ -26,7 +19,7 @@ if (module.hot) {
     ReactDOM.render(<Provider store={store}><Routers /></Provider>, rootEl);
   });
 }
->>>>>>> react-redux-demo
+
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
