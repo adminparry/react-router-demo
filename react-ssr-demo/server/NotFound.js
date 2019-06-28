5f0c3d257c8b3c7a3a80ec1e5d9bc0f1b9736485
pparry <1170015014@qq.com>
2019-06-28 16:22:53 +0800

    config

---
+++ react-ssr-demo/server/NotFound.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import './test.css';
+const NotFound = () => {
+  return (
+    <h1 className="bg">404 not found...</h1>
+  );
+}
+
+export default NotFound;
---
+++ react-ssr-demo/src/routers/index.js
@@ -0,0 +1,42 @@
+import React from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
+import Loadable from 'react-loadable'
+import Loading from './Loading';
+
+const NoFound = Loadable({
+  loader: () => import(/* webpackChunkName:"NotFound" */'./NotFound'),
+  loading: Loading
+})
+const Home = Loadable({
+  loader: () => import(/* webpackChunkName:"Home" */'../components/home'),
+  loading: Loading
+})
+const Foo = Loadable({
+  loader: () => import(/* webpackChunkName:"Foo" */'../components/foo'),
+  loading: Loading
+})
+const About = Loadable({
+  loader: () => import(/* webpackChunkName:"About" */'../components/about'),
+  loading: Loading
+})
+
+const Routers = () => {
+  return (
+    <Router>
+        <Switcher />
+    </Router>
+  );
+}
+export const Switcher = () => {
+  return (
+    <Switch>
+      <Route exact path="/" component={ Home } ></Route>
+
+      <Route exact path="/foo" component={ Foo } />
+      <Route exact path="/about" component={ About } />
+
+      <Route component={NoFound} />
+    </Switch>
+  )
+}
+export default Routers;
