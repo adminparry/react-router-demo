1c86a59f60af9cbd954c30db364669e89297c980
pparry <1170015014@qq.com>
2019-09-09 19:06:42 +0800

    feat: test setState

--- react-router-demo/src/components/foo/index.js
+++ react-router-demo/src/components/foo/index.js
@@ -1,9 +1,32 @@
 import React from 'react';
 
 class Home extends React.Component {
+	constructor(props){
+		super(props);
+		this.state = {
+			a: 4
+		}
+
+	}
+
+	componentDidMount(){
+		this.setState({a: 5});
+		this.setState({a: 6});
+		this.setState({a: 7});
+
+		console.log(this.state.a)
+	}
+	handlerClick(e) {
+		console.log(e)
+		alert("success")
+	}
 	render(){
+		console.log("render", this.state.a)
 		return (
-			<div> this is foo </div>
+			<div>
+				<span> this is foo </span>
+				<button onClick={ this.handlerClick}>双冒号点击测试</button>
+			</div>
 		)
 	}
 }
